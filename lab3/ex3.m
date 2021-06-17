1706f21d5ae43b0f187a73f2e630088fc9b2ccc3
Pedro <pedralmeida.22.99@gmail.com>
2021-06-17 19:47:23 +0100

    afinal faltava a h)

--- lab3/ex3.m
+++ lab3/ex3.m
@@ -2,8 +2,8 @@ lambda = [1500, 1600, 1700, 1800, 1900, 2000];
 C = 10;
 P = 10000;
 f = 10000000;
-b = 0; % b)
-%b = 1e-5; % g
+%b = 0; % b)
+b = 1e-5; % g
 
 % number of simulations
 %N = 20; % a
@@ -28,7 +28,7 @@ for i=1:size(lambda, 2)
        [PL(it), APD(it), MPD(it), TT(it)] = simulator2(lambda(i), C, f, P, b);
     end
 
-    % 90confidence interval %
+    % 90 confidence interval %
     alfa= 0.1; 
 
     mediaPL(i) = mean(PL);
@@ -44,7 +44,19 @@ for i=1:size(lambda, 2)
     termTT(i) = norminv(1-alfa/2)*sqrt(var(TT)/N);
 end
 
+% graficos
 figure(1)
+bar(lambda, mediaPL)
+title('Packet Loss')
+xlabel('Request/Hour')
+ylabel('(%%)')
+hold on
+er = errorbar(lambda, mediaPL, termPL, termPL);
+er.LineStyle = 'none';  
+hold off
+grid on
+
+figure(2)
 bar(lambda, mediaAPD)
 title('Average Packet Delay')
 xlabel('Request/Hour')
@@ -55,7 +67,7 @@ er.LineStyle = 'none';
 hold off
 grid on
 
-figure(2)
+figure(3)
 bar(lambda, mediaMPD)
 title('Maximum Packet Delay')
 xlabel('Request/Hour')
@@ -66,7 +78,7 @@ er.LineStyle = 'none';
 hold off
 grid on
 
-figure(3)
+figure(4)
 bar(lambda, mediaTT)
 title('Total Throughput')
 xlabel('Request/Hour')
@@ -89,9 +101,7 @@ f = 10000000;
 b = 0;
 
 % number of simulations
-%N = 20; % a
 N = 40; % b
-N = 5;
 
 APD = zeros(1,N);
 TT = zeros(1,N);
@@ -203,8 +213,7 @@ for i=1:size(lambda, 2)
 end
 
 % plots
-figure(4)
-subplot(2,1,1)
+figure(1)
 bar(lambda, [mediaAPD; APD_mm1; APD_mg1])
 title('Average Packet Delay')
 xlabel('Request/Hour')
@@ -212,7 +221,7 @@ ylabel('(ms)')
 legend('Sim', 'MM1', 'MG1', 'Location', 'northwest')
 grid on
 
-subplot(2,1,2)
+figure(2)
 bar(lambda, [mediaTT; TT_mm1; TT_mg1])
 title('Total Throughput')
 xlabel('Request/Hour')
@@ -227,8 +236,8 @@ lambda = 1800;
 C = 10;
 P = 10000;
 f = [2500, 5000, 7500, 10000, 12500, 15000, 17500, 20000];
-b = 0; % d)
-% b = 1e-5; % f)
+%b = 0; % d)
+b = 1e-5; % f)
 
 % number of simulations
 N = 40;
@@ -250,7 +259,7 @@ for i=1:size(f, 2)
        [PL(it), APD(it), MPD(it), TT(it)] = simulator2(lambda, C, f(i), P, b);
     end
 
-    % 90confidence interval %
+    % 90 confidence interval %
     alfa= 0.1; 
 
     mediaPL(i) = mean(PL);
@@ -267,6 +276,17 @@ for i=1:size(f, 2)
 end
 
 figure(1)
+bar(f, mediaPL)
+title('Packet Loss')
+xlabel('Queue size (Bytes)')
+ylabel('(%%)')
+hold on
+er = errorbar(f, mediaPL, termPL, termPL);
+er.LineStyle = 'none';  
+hold off
+grid on
+
+figure(2)
 bar(f, mediaAPD)
 title('Average Packet Delay')
 xlabel('Queue size (Bytes)')
@@ -277,7 +297,7 @@ er.LineStyle = 'none';
 hold off
 grid on
 
-figure(2)
+figure(3)
 bar(f, mediaMPD)
 title('Maximum Packet Delay')
 xlabel('Queue size (Bytes)')
@@ -288,7 +308,7 @@ er.LineStyle = 'none';
 hold off
 grid on
 
-figure(3)
+figure(4)
 bar(f, mediaTT)
 title('Total Throughput')
 xlabel('Queue size (Bytes)')
@@ -311,7 +331,6 @@ b = 0;
 disp('Simulation started');
 % number of simulations
 N = 40;
-N=5;
 
 PL = zeros(1,N); 
 APD = zeros(1,N);
@@ -375,7 +394,6 @@ end
 
 
 % plots
-
 figure(6)
 bar(f, [mediaPL; PL_mm1m])
 title('Packet Loss')
@@ -426,4 +444,119 @@ grid on
 % legend('Sim', 'MM1M', 'Location', 'northwest')
 % grid on
 
+%%
+%h
+lambda = [1500, 1600, 1700, 1800, 1900, 2000];
+C = 10 * 1e6;
+P = 10000;
+f = 10000000;
+b = 1e-5; % g
 
+% number of simulations
+N = 40; % b
+
+PL = zeros(1,N); 
+APD = zeros(1,N);
+MPD = zeros(1,N);
+TT = zeros(1,N);
+
+mediaPL = zeros(1, size(lambda, 2));
+mediaAPD = zeros(1, size(lambda, 2));
+mediaMPD = zeros(1, size(lambda, 2));
+mediaTT = zeros(1, size(lambda, 2));
+
+disp('Simulation started...');
+for i=1:size(lambda, 2)
+    for it=1:N
+       [PL(it), APD(it), MPD(it), TT(it)] = simulator2(lambda(i), 10, f, P, b);
+    end
+
+    mediaPL(i) = mean(PL);
+    mediaAPD(i) = mean(APD);
+    mediaMPD(i) = mean(MPD);
+    mediaTT(i) = mean(TT);
+
+end
+
+% M/G/1 queueing model
+disp('M/G/1 started...');
+
+P64 = (1 - b)^(8*64);
+P110 = (1 - b)^(8*110);
+P1518 = (1 - b)^(8*1518);
+
+% avg packet delay
+es64 = 0.16 * ((8 * 64) / C);
+es110 = 0.25 * ((8 * 110) / C);
+es1518 = 0.20 * ((8 * 1518) / C);
+ess64 = 0.16 * ((8 * 64) / C)^2;
+ess110 = 0.25 * ((8 * 110) / C)^2;
+ess1518 = 0.20 * ((8 * 1518) / C)^2;
+
+es = es64 + es110 + es1518;
+ess = ess64 + ess110 + ess1518;
+
+for i=1:length(tam)
+    es = es + (pres * ((8 * tam(i)) / C));
+    ess = ess + (pres * ((8 * tam(i)) / C)^2);
+end
+
+
+APD_mg1 = zeros(1, size(lambda, 2));
+TT_mg1 = zeros(1, size(lambda, 2));
+
+for i=1:size(lambda, 2)
+    wq = (lambda(i) * ess) / (2 * (1 - lambda(i) * es));
+    
+    wi64 = wq + ((8*64)/C);
+    wi110 = wq + ((8*110)/C);
+    wi1518 = wq + ((8*1518)/C);
+
+    apdUp64 = 0.16 * P64 * wi64;
+    apdUp110 = 0.25 * P110 * wi110;
+    apdUp1518 = 0.20 * P1518 * wi1518;
+    apdDown64 = 0.16 * P64;
+    apdDown110 = 0.25 * P110;
+    apdDown1518 = 0.20 * P1518;
+
+    apdUp = apdUp64 + apdUp110 + apdUp1518;
+    apdDown = apdDown64 + apdDown110 + apdDown1518;
+
+    for it=1:length(tam)
+        Pi = (1 - b)^(8*tam(it));
+        wi = wq + ((8*tam(it))/C);
+        apdUp = apdUp + pres * Pi * wi;
+        apdDown = apdDown + pres * Pi;
+    end
+
+    APD_mg1(i) = 1e3 * (apdUp / apdDown);
+    
+    % total throughput
+    T64= 0.16 * P64 * lambda(i)*8*64;
+    T110= 0.25 * P110 * lambda(i)*8*110;
+    T1518= 0.20 * P1518 * lambda(i)*8*1518;
+    TT_mg1(i)= (T64 + T110 + T1518);
+
+    for it=1:length(tam)
+        Pi = (1 - b)^(8*tam(it));
+        TT_mg1(i)= (TT_mg1(i) + (pres * Pi * lambda(i) * (8*tam(it))));
+    end
+    TT_mg1(i)= TT_mg1(i) / 1e6;
+end
+
+% graficos
+figure(1)
+bar(lambda, [mediaAPD; APD_mg1])
+title('Average Packet Delay')
+xlabel('Request/Hour')
+ylabel('(ms)')
+legend('Sim', 'MG1', 'Location', 'northwest')
+grid on
+
+figure(2)
+bar(lambda, [mediaTT; TT_mg1])
+title('Total Throughput')
+xlabel('Request/Hour')
+ylabel('(Mbps)')
+legend('Sim', 'MG1', 'Location', 'northwest')
+grid on
