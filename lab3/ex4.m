389e661633f7198b36d9c5d23ca7c0969f99d8d5
Pedro <pedralmeida.22.99@gmail.com>
2021-06-21 20:19:45 +0100

    g3 ex4 done

--- lab3/ex4.m
+++ lab3/ex4.m
@@ -1,10 +1,11 @@
 % INPUT PARAMETERS:
 lambda = 1800;  % lambda - packet rate (packets/sec)
 C = 10;         % link bandwidth (Mbps)
-f = 10e6; % a
-% f = 10e4 % b
 P = 100000;     % number of packets (stopping criterium)
-b = 0;
+%f = 1e6; % a
+%b = 0;   % a
+f = 1e4; % b
+b = 1e-5; % b
 
 % OUTPUT PARAMETERS:
 %  PL   - packet loss (%)
@@ -38,7 +39,152 @@ termMPD = norminv(1-alfa/2)*sqrt(var(MPD)/N);
 mediaTT = mean(TT);
 termTT = norminv(1-alfa/2)*sqrt(var(TT)/N);
     
-fprintf('\nPacket Loss: %.4f + %.4f\n', mediaPL, termPL);
-fprintf('Avg packet delay (ms): %.4f + %.4f\n', mediaAPD, termAPD);
-fprintf('Max packet delay (ms): %.4f + %.4f\n', mediaMPD, termMPD);
-fprintf('Throughtput (Mbps): %.4f +- %.4f\n', mediaTT, termTT);
+fprintf('\nPacket Loss: %.4e + %.4e\n', mediaPL, termPL);
+fprintf('Avg packet delay (ms): %.4e + %.4e\n', mediaAPD, termAPD);
+fprintf('Max packet delay (ms): %.4e + %.4e\n', mediaMPD, termMPD);
+fprintf('Throughtput (Mbps): %.4e +- %.4e\n', mediaTT, termTT);
+
+%%
+% 4.c
+lambda = [1500, 1600, 1700, 1800, 1900, 2000];  % lambda - packet rate (packets/sec)
+C = 10;         % link bandwidth (Mbps)
+P = 100000;     % number of packets (stopping criterium)
+f = 1e7;
+%b = 0; % c)
+b = 1e-5; % e)
+
+mediaPL = zeros(1, size(lambda, 2));
+mediaAPD = zeros(1, size(lambda, 2));
+mediaMPD = zeros(1, size(lambda, 2));
+mediaTT = zeros(1, size(lambda, 2));
+
+mediaPL3 = zeros(1, size(lambda, 2));
+mediaAPD3 = zeros(1, size(lambda, 2));
+mediaMPD3 = zeros(1, size(lambda, 2));
+mediaTT3 = zeros(1, size(lambda, 2));
+
+for i=1:size(lambda, 2)
+    for it=1:N
+       [PL(it), APD(it), MPD(it), TT(it)] = simulator2(lambda(i), C, f, P, b);
+    end
+    
+    mediaPL(i) = mean(PL);
+    mediaAPD(i) = mean(APD);
+    mediaMPD(i) = mean(MPD);
+    mediaTT(i) = mean(TT);
+    
+    for it=1:N
+       [PL(it), APD(it), MPD(it), TT(it)] = simulator3(lambda(i), C, f, P, b);
+    end
+    
+    mediaPL3(i) = mean(PL);
+    mediaAPD3(i) = mean(APD);
+    mediaMPD3(i) = mean(MPD);
+    mediaTT3(i) = mean(TT);
+end
+
+% plots
+figure(1)
+bar(lambda, [mediaPL; mediaPL3])
+title('Packet Loss')
+xlabel('Request/Hour')
+ylabel('(%%)')
+legend('Sim2', 'Sim3', 'Location', 'northwest')
+grid on
+
+figure(2)
+bar(lambda, [mediaAPD; mediaAPD3])
+title('Average Packet Delay')
+xlabel('Request/Hour')
+ylabel('(ms)')
+legend('Sim2', 'Sim3', 'Location', 'northwest')
+grid on
+
+figure(3)
+bar(lambda, [mediaMPD; mediaMPD3])
+title('Maximum Packet Delay')
+xlabel('Request/Hour')
+ylabel('(ms)')
+legend('Sim2', 'Sim3', 'Location', 'northwest')
+grid on
+
+figure(4)
+bar(lambda, [mediaTT; mediaTT3])
+title('Throughput')
+xlabel('Request/Hour')
+ylabel('(Mbps)')
+legend('Sim2', 'Sim3', 'Location', 'northwest')
+grid on
+
+%% 
+% 4 d)
+lambda = 1800;  % lambda - packet rate (packets/sec)
+C = 10;         % link bandwidth (Mbps)
+P = 100000;     % number of packets (stopping criterium)
+f = [2500, 5000, 7500, 10000, 12500, 15000, 17500, 20000]; 
+b = 0; % d)
+%b = 1e-5; % f)
+
+mediaPL = zeros(1, size(f, 2));
+mediaAPD = zeros(1, size(f, 2));
+mediaMPD = zeros(1, size(f, 2));
+mediaTT = zeros(1, size(f, 2));
+
+mediaPL3 = zeros(1, size(f, 2));
+mediaAPD3 = zeros(1, size(f, 2));
+mediaMPD3 = zeros(1, size(f, 2));
+mediaTT3 = zeros(1, size(f, 2));
+
+for i=1:size(f, 2)
+    for it=1:N
+       [PL(it), APD(it), MPD(it), TT(it)] = simulator2(lambda, C, f(i), P, b);
+    end
+    
+    mediaPL(i) = mean(PL);
+    mediaAPD(i) = mean(APD);
+    mediaMPD(i) = mean(MPD);
+    mediaTT(i) = mean(TT);
+    
+    for it=1:N
+       [PL(it), APD(it), MPD(it), TT(it)] = simulator3(lambda, C, f(i), P, b);
+    end
+    
+    mediaPL3(i) = mean(PL);
+    mediaAPD3(i) = mean(APD);
+    mediaMPD3(i) = mean(MPD);
+    mediaTT3(i) = mean(TT);
+end
+
+% plots
+figure(1)
+bar(f, [mediaPL; mediaPL3])
+title('Packet Loss')
+xlabel('Queue size (Bytes)')
+ylabel('(%%)')
+legend('Sim2', 'Sim3', 'Location', 'northwest')
+grid on
+
+figure(2)
+bar(f, [mediaAPD; mediaAPD3])
+title('Average Packet Delay')
+xlabel('Queue size (Bytes)')
+ylabel('(ms)')
+legend('Sim2', 'Sim3', 'Location', 'northwest')
+grid on
+
+figure(3)
+bar(f, [mediaMPD; mediaMPD3])
+title('Maximum Packet Delay')
+xlabel('Queue size (Bytes)')
+ylabel('(ms)')
+legend('Sim2', 'Sim3', 'Location', 'northwest')
+grid on
+
+figure(4)
+bar(f, [mediaTT; mediaTT3])
+title('Throughput')
+xlabel('Queue size (Bytes)')
+ylabel('(Mbps)')
+legend('Sim2', 'Sim3', 'Location', 'northwest')
+grid on
+
--- lab3/simulator3.m
+++ lab3/simulator3.m
@@ -13,11 +13,15 @@ function [PL , APD , MPD , TT] = Simulator3(lambda,C,f,P,b)
 
  denominador = (1 + (10/5) + ((10/5)*(5/10)));
  
+ % prob de cada estado
  s1 = 1 / denominador;
  s2 = (10/5) / denominador;
  s3 = ((10/5)*(5/10)) / denominador; 
  
- p_states = [s1 s2 s3];
+ % tempo de permanencia em cada estado
+ t1 = 1 / 10;
+ t2 = 1 / (5 + 5);
+ t3 = 1 / 10;
 
 %Events:
 ARRIVAL= 0;         % Arrival of a packet            
@@ -28,9 +32,6 @@ TRANSITION= 2;      % transition of a state in the packet arriving Markov chain
 STATE = 0;          % 0 - connection free; 1 - connection bysy
 QUEUEOCCUPATION= 0; % Occupation of the queue (in Bytes)
 QUEUE= [];          % Size and arriving time instant of each packet in the queue
-% current state of the packet arriving Markov chain 
-% initialized according to the probability of each state
-FlowState= 0;   
 
 %Statistical Counters:
 TOTALPACKETS= 0;       % No. of packets arrived to the system
@@ -44,8 +45,28 @@ MAXDELAY= 0;           % Maximum delay among all transmitted packets
 % Initializing the simulation clock:
 Clock= 0;
 
+rate = 0;   % "lambda" consoante o estado
+
+temp = rand;
+% escolhe estado inicial, atualiza o rate adequado ao estado e "agenda" o
+% novo evento de transicao
+if temp <= s1
+    FlowState= 1;
+    rate = lambda * 0.5;
+    EventList = [TRANSITION, Clock + exprnd(t1), 0, 0];
+elseif temp <= s1 + s2
+    FlowState= 2;
+    rate = lambda;
+    EventList = [TRANSITION, Clock + exprnd(t2), 0, 0];
+else
+    FlowState= 3;
+    rate = lambda * 1.5;
+    EventList = [TRANSITION, Clock + exprnd(t3), 0, 0];
+end
+
 % Initializing the List of Events with the first ARRIVAL:
-EventList = [ARRIVAL, Clock + exprnd(1/lambda), GeneratePacketSize(), 0];
+EventList = [EventList; ARRIVAL, Clock + exprnd(1/rate), GeneratePacketSize(), 0];
+
 
 %Similation loop:
 while TRANSMITTEDPACKETS<P               % Stopping criterium
@@ -58,7 +79,7 @@ while TRANSMITTEDPACKETS<P               % Stopping criterium
     switch Event
         case ARRIVAL                     % If first event is an ARRIVAL
             TOTALPACKETS= TOTALPACKETS+1;
-            EventList = [EventList; ARRIVAL, Clock + exprnd(1/lambda), GeneratePacketSize(), 0];
+            EventList = [EventList; ARRIVAL, Clock + exprnd(1/rate), GeneratePacketSize(), 0];
             if STATE==0
                 STATE= 1;
                 EventList = [EventList; DEPARTURE, Clock + 8*PacketSize/(C*10^6), PacketSize, Clock];
@@ -93,7 +114,28 @@ while TRANSMITTEDPACKETS<P               % Stopping criterium
             end
             
         case TRANSITION     % If first event is a TRANSITION
-            % do something 
+            % se o estado é diferente do 2, entao o novo estado é o dois
+            % atualiza o rate adequado ao estado 
+            % e "agenda" novo evento de transicao
+            if FlowState ~= 2   % transiçao para os estados 1 e 3
+                % so pode ir para s2
+                FlowState = 2;
+                rate = lambda;
+                EventList = [EventList; TRANSITION, Clock + exprnd(t2), 0, 0];
+            
+            else % FlowState == 2
+                % decidir se vai para s1 ou s3
+                temp = rand;
+                if temp < 0.5   % taxa de transicao e igual para os dois estados possiveis (ou seja 50-50) 
+                    FlowState = 1;
+                    EventList = [EventList; TRANSITION, Clock + exprnd(t1), 0, 0];
+                    rate = lambda * 0.5;
+                else
+                    FlowState = 3;
+                    EventList = [EventList; TRANSITION, Clock + exprnd(t3), 0, 0];
+                    rate = lambda * 1.5;
+                end
+            end
     end
 end
 
